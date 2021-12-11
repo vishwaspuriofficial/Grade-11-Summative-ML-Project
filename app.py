6ef730398778be740fb74d821e145a5d2e90a418
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-12-11 00:07:22 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import mediapipe as mp
 import cv2
+from deepface import DeepFace
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # st.set_page_config(layout="wide")
 col = st.empty()
@@ -8,6 +10,13 @@ col = st.empty()
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
+
+noseCascade = cv2.CascadeClassifier("haarcascade_mcs_nose.xml")
+mouthCascade = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
 
 from streamlit_webrtc import (
     AudioProcessorBase,
@@ -40,6 +49,27 @@ def webcam(page):
                 if results.multi_hand_landmarks:
                     for handLms in results.multi_hand_landmarks:
                         mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+            elif page == "poseDetector":
+                imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                results = pose.process(imgRGB)
+                mpDraw.draw_landmarks(
+                    img,
+                    results.pose_landmarks,
+                    mpPose.POSE_CONNECTIONS)
+
+            elif page == "facialSentimentAnalysis":
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+                faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+                cv2.rectangle(img, pt1=(0, 0), pt2=(700, 80), color=(0, 0, 0), thickness=-1)
+                try:
+                    predictions = DeepFace.analyze(img, actions=['emotion'])
+                    for (x, y, w, h) in faces:
+                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    cv2.putText(img, predictions['dominant_emotion'], (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, 255)
+                except:
+                    cv2.putText(img, "No Face Detected!", (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, 255)
+                return av.VideoFrame.from_ndarray(img, format="bgr24")
 
             elif page == "handSignRecognizer":
                 imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -102,19 +132,22 @@ def webcam(page):
 
                             else:
                                 cv2.putText(img, "Hand Detected!", (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, 255)
+
+                elif page == "maskDetector":
+                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    nose = noseCascade.detectMultiScale(gray, 1.3, 5)
+                    mouth = mouthCascade.detectMultiScale(gray, 1.3, 5)
+                    cv2.rectangle(img, pt1=(0, 0), pt2=(700, 80), color=(0, 0, 0), thickness=-1)
+                    if len(nose) != 0 and len(mouth) != 0:
+                        cv2.putText(img, "Not Wearing Mask", (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, 255)
+                    else:
+                        cv2.putText(img, "Good, Wearing Mask!", (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, 255)
+
                 else:
                     cv2.putText(img, "No Hand Detected!", (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, 255)
 
             return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-    webrtc_ctx = webrtc_streamer(
-        key="opencv-filter",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration=RTC_CONFIGURATION,
-        video_processor_factory=OpenCVVideoProcessor,
-        media_stream_constraints={"video": True, "audio": False},
-        async_processing=True,
-    )
     # webrtc_ctx = webrtc_streamer(
     #     key="opencv-filter",
     #     mode=WebRtcMode.SENDRECV,
@@ -122,12 +155,20 @@ def webcam(page):
     #     video_processor_factory=OpenCVVideoProcessor,
     #     media_stream_constraints={"video": True, "audio": False},
     #     async_processing=True,
-    #     video_html_attrs={
-    #         "style": {"margin": "0 auto", "border": "5px yellow solid"},
-    #         "controls": False,
-    #         "autoPlay": True,
-    #     },
     # )
+    webrtc_ctx = webrtc_streamer(
+        key="opencv-filter",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_processor_factory=OpenCVVideoProcessor,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,
+        video_html_attrs={
+            "style": {"margin": "0 auto", "border": "5px yellow solid"},
+            "controls": False,
+            "autoPlay": True,
+        },
+    )
 
 
 page = ""
